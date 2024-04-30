21fd4a6ada89bad9d404ae4cb1a67866dd51ea09
Diecho <d.hidalgo.pe@gmail.com>
2024-04-29 23:31:58 -0400

    some

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+// import interact from "./node_modules/interactjs/package.json"
 
-import interact from ""
+interact('.item2').draggable({
 
-interact('.item').draggable({
+
+  inertia: true,
+  // keep the element within the area of it's parent
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: 'parent',
+      endOnly: true
+    })
+  ],
+  // enable autoScroll
+  autoScroll: true,
+
     listeners: {
       move (event) {
-        console.log(event.pageX,
-                    event.pageY)
-      }
+        console.log(event.pageX, event.pageY);
+        var target = event.target
+        // keep the dragged position in the data-x/data-y attributes
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+      
+        // translate the element
+        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+      
+        // update the posiion attributes
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)          
+      },
     }
   })
   
